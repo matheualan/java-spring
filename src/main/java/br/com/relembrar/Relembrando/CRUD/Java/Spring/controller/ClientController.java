8e59fa7a7b2443c8c935c6c4c26b15de98516dc0
maths <matheusalan.dev@gmail.com>
2026-03-21 02:16:43 -0300

    Implementacao metodos POST e GET na camada service e controller da entidade Client.

--- src/main/java/br/com/relembrar/Relembrando/CRUD/Java/Spring/controller/ClientController.java
+++ src/main/java/br/com/relembrar/Relembrando/CRUD/Java/Spring/controller/ClientController.java
@@ -1,17 +1,20 @@
 package br.com.relembrar.Relembrando.CRUD.Java.Spring.controller;
 
-import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientPostDTO;
+import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientRequest;
+import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientResponse;
 import br.com.relembrar.Relembrando.CRUD.Java.Spring.model.Client;
 import br.com.relembrar.Relembrando.CRUD.Java.Spring.service.ClientService;
+import io.swagger.v3.oas.annotations.Hidden;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(path = "/products")
+@RequestMapping(path = "/clients")
 public class ClientController {
 
     private final ClientService clientService;
@@ -20,24 +23,41 @@ public class ClientController {
         this.clientService = clientService;
     }
 
+    @PostMapping(path = "/")
+    public ResponseEntity<ClientRequest> saveProduct(@RequestBody @Valid ClientRequest clientRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.saveClient(clientRequest));
+    }
+
+    @PostMapping(path = "/all")
+    public ResponseEntity<List<ClientRequest>> saveMultipleClients(@RequestBody @Valid List<ClientRequest> listClients) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.saveMultipleClients(listClients));
+    }
+
+    @GetMapping(path = "/clientById")
+    public ResponseEntity<Optional<ClientResponse>> findClientById(@RequestParam Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.findClientById(id));
+    }
+
+    @GetMapping(path = "/listResponse")
+    public ResponseEntity<List<ClientResponse>> listClientResponse() {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.listClientResponse());
+    }
+
     @GetMapping(path = "/list")
     public ResponseEntity<List<Client>> findAll() {
         return ResponseEntity.ok().body(clientService.findAll());
     }
 
-    @GetMapping(path = "/list-record")
-    public ResponseEntity<List<ClientPostDTO>> findAllRecords() {
-        return ResponseEntity.status(HttpStatus.OK).body(clientService.findAllRecords());
-    }
-
     @GetMapping(path = "/list-dto")
-    public ResponseEntity<List<ClientPostDTO>> findAllDto() {
+    public ResponseEntity<List<ClientRequest>> findAllDto() {
         return ResponseEntity.status(HttpStatus.OK).body(clientService.findAllDto());
     }
 
-    @PostMapping(path = "/save")
-    public ResponseEntity<ClientPostDTO> saveProduct(@RequestBody @Valid ClientPostDTO clientPostDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.saveClient(clientPostDto));
+    @Hidden
+    @GetMapping(path = "/list-record")
+    public ResponseEntity<List<ClientRequest>> findAllRecords() {
+        return ResponseEntity.status(HttpStatus.OK).body(clientService.findAllRecords());
     }
+
 
 }
--- src/main/java/br/com/relembrar/Relembrando/CRUD/Java/Spring/service/ClientService.java
+++ src/main/java/br/com/relembrar/Relembrando/CRUD/Java/Spring/service/ClientService.java
@@ -1,12 +1,16 @@
 package br.com.relembrar.Relembrando.CRUD.Java.Spring.service;
 
-import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientPostDTO;
+import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientRequest;
+import br.com.relembrar.Relembrando.CRUD.Java.Spring.dto.ClientResponse;
+import br.com.relembrar.Relembrando.CRUD.Java.Spring.exceptions.ClientNotFoundException;
 import br.com.relembrar.Relembrando.CRUD.Java.Spring.model.Client;
 import br.com.relembrar.Relembrando.CRUD.Java.Spring.repository.ClientRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ClientService {
@@ -17,14 +21,51 @@ public class ClientService {
         this.clientRepository = clientRepository;
     }
 
+    @Transactional()
+    public ClientRequest saveClient(ClientRequest clientRequest) {
+        Client client = new Client(
+                clientRequest.name(),
+                clientRequest.email(),
+                clientRequest.password()
+        );
+        clientRepository.save(client);
+        return clientRequest;
+    }
+
+    @Transactional
+    public List<ClientRequest> saveMultipleClients(List<ClientRequest> listClientsDTO) {
+        List<Client> clients = new ArrayList<>();
+        for (ClientRequest clientDto : listClientsDTO) {
+            clients.add(new Client(clientDto.name(), clientDto.email(), clientDto.password()));
+        }
+        clientRepository.saveAll(clients);
+        return listClientsDTO;
+    }
+
     public List<Client> findAll() {
         return clientRepository.findAll();
     }
 
-    public List<ClientPostDTO> findAllRecords() {
+    public Optional<ClientResponse> findClientById(Long id) {
+        if (id == null) return Optional.empty();
+
+        Client client = clientRepository.findById(id).get();
+
+        return Optional.of(new ClientResponse(client.getName(),
+                client.getEmail()));
+    }
+
+    public Client getClientById(Long id) {
+        if (id == null) throw new IllegalArgumentException("ID não pode ser nulo.");
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new ClientNotFoundException("Cliente não encontrado com o ID: " + id));
+    }
+
+//    So para saber que existe essa forma
+    public List<ClientRequest> findAllRecords() {
         return findAll()
                 .stream()
-                .map(p -> new ClientPostDTO(
+                .map(p -> new ClientRequest(
                         p.getName(),
                         p.getEmail(),
                         p.getPassword()
@@ -32,22 +73,18 @@ public class ClientService {
                 .toList();
     }
 
-    public List<ClientPostDTO> findAllDto() {
+    public List<ClientRequest> findAllDto() {
         return findAll()
                 .stream()
-                .map(Client::toDTO)
+                .map(Client::toRequestDTO)
                 .toList();
     }
 
-    @Transactional()
-    public ClientPostDTO saveClient(ClientPostDTO clientPostDto) {
-        Client client = new Client(
-                clientPostDto.name(),
-                clientPostDto.email(),
-                clientPostDto.password()
-        );
-        clientRepository.save(client);
-        return clientPostDto;
+    public List<ClientResponse> listClientResponse() {
+        return clientRepository.findAll()
+                .stream()
+                .map(Client::toResponseDTO)
+                .toList();
     }
 
 }
